df57dc4bb9f3ec59450ed9ac0f987534caac3e9c
deynekaY <e.deyneka@imagecms.net>
2022-08-25 13:13:23 +0300

    home work aploaded

--- 02-homework/scripts/script.js
+++ 02-homework/scripts/script.js
@@ -1,14 +1,37 @@
-let n = prompt('Введіть число', '');
-if (n > 0) {
-    alert('Число прийнято!');
-} else if (n < 0) {
-    prompt('Ви ввели невірне число', '');
-} else if (n == '') {
-    prompt('Ви забули ввести число', '');
-} else {
-    prompt('Ви не ввели число - ведіть число', '');
-}
-
-
-
-// document.writeln('<p><b>Чистий прибуток, якщо клієнт заплатив зі знижкою та собівартість товарів рівно в два рази нижче їх ціни</b> = ' + clearEarn() + '</p>');
+function getNumberFromUser(numberName) {
+    let number;
+    while (!number) {
+      number = +prompt(`Введіть число ${numberName}`);
+      if (number === 0) {
+        break;
+      }
+      if (!number) {
+        alert("Це не число");
+      }
+      if (!Number.isInteger(number)) {
+        alert("Введіть ціле число");
+        number = null;
+      }
+    }
+    return number;
+  }
+  
+  const number_n = getNumberFromUser("N");
+  const number_m = getNumberFromUser("M");
+  
+  const skipEven = confirm("Пропустити парні числа?");
+  
+  let sum = 0;
+  
+  for (let i = number_n; i <= number_m; i++) {  
+      sum += skipEven ? ( !(i % 2) ? 0 : i) : i;
+  }
+  
+  console.log(sum);
+  
+  document.getElementById("info").innerHTML = `
+  <p> Число N = ${number_n}</p>
+  <p> Число M = ${number_m}</p>
+  <p> Пропустити парні числа = ${skipEven}</p>
+  <p> Сума = ${sum}</p>
+  `;
