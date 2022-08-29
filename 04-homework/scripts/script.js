8577c1d80c19e353b4a725c3d100b04c796c79b9
deynekaY <e.deyneka@imagecms.net>
2022-08-29 11:25:21 +0300

    #4 in progress

--- 04-homework/scripts/script.js
+++ 04-homework/scripts/script.js
@@ -1,10 +1,25 @@
-const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+let students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
 
-const themes = [
+let themes = [
   "Диференційне рівняння",
   "Теорія автоматів",
   "Алгоритми і структури даних",
 ];
 
-const marks = [4, 5, 5, 3, 4, 5];
+let marks = [4, 5, 5, 3, 4, 5];
 
+students[6] = students[0];
+
+function pairs(students, size = 2) {
+  return students
+    .map((x, i) => i % size == 1 && students.slice(i, i + size))
+    .filter((x) => x);
+}
+// document.writeln(
+//   `<br /><br /><b>Пари студентів</b> - <b>${pairs(students)}</b>`
+// );
+console.log(pairs(students));
+
+
+let studentsThemes = pairs(students) + themes;
+console.log(studentsThemes);
