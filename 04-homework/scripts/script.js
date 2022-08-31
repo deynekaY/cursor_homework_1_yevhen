899b6de7aa8360b37c7ddd4b165c27fb55ee8227
deynekaY <e.deyneka@imagecms.net>
2022-08-31 09:47:33 +0300

    hw #4 ended

--- 04-homework/scripts/script.js
+++ 04-homework/scripts/script.js
@@ -8,21 +8,46 @@ let themes = [
 
 let marks = [4, 5, 5, 3, 4, 5];
 
-students[6] = students[0];
-
 function pairs(students, size = 2) {
+  students[6] = students[0];
   return students
     .map((x, i) => i % size == 1 && students.slice(i, i + size))
     .filter((x) => x);
 }
 console.log(pairs(students));
 
-let studentsPairs = [pairs(students)];
+let teamProject = [];
 
-function studentsPairsThemes(studentsPairs, themes) {
-  for (let i = 0; studentsPairs.length;)
-  themes.splice(i++ * 2, 0, studentsPairs.shift());
-  return themes;
+function studentsPairsThemes(pairs, themes) {
+  for (let i = 0; i < pairs.length; i++) {
+    teamProject.push([pairs[i], themes[i]].flat());
+  }
+  return teamProject;
 }
 
-console.log(studentsPairsThemes(studentsPairs, themes));
+let pairsProject = studentsPairsThemes(pairs(students), themes);
+
+console.log(pairsProject);
+
+let studentsMarks = [];
+
+students.pop();
+
+for (var i = 0; i < students.length; i++) {
+  studentsMarks.push(students[i]);
+  studentsMarks.push(marks[i]);
+}
+console.log(studentsMarks);
+
+let teamProjectMarks = [];
+
+function studentsPairsThemesMarks(pairsProject, marks) {
+  for (let i = 0; i < pairsProject.length; i++) {
+    teamProjectMarks.push([pairsProject[i], marks[i]].flat());
+  }
+  return teamProjectMarks;
+}
+
+let pairsProjectMarks = studentsPairsThemesMarks(pairsProject, marks);
+
+console.log(pairsProjectMarks);
